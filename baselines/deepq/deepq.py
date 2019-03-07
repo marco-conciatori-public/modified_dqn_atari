cd47e854b20782d4b2f826e9542c60dab371d414
Marco Conciatori <irotaicnoc@gmail.com>
2019-03-07 15:45:29 +0100

    test

--- baselines/deepq/deepq.py
+++ baselines/deepq/deepq.py
@@ -76,9 +76,9 @@ class ActWrapper(object):
         with open(path, "wb") as f:
             cloudpickle.dump((model_data, self._act_params, train_params), f)
 
-        from google.colab import files
-        temp_path = os.path.join(logger.get_dir(), path)
-        files.download(temp_path)
+        # from google.colab import files
+        # temp_path = os.path.join(logger.get_dir(), path)
+        # files.download(temp_path)
 
     def save(self, path):
         save_variables(path)
