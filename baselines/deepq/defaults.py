31fb7e324b03ec821b68a0019bb312ce57098b94
Marco Conciatori <irotaicnoc@gmail.com>
2019-03-10 13:48:49 +0100

    some parameters unlocked, to choose from command line

--- baselines/deepq/defaults.py
+++ baselines/deepq/defaults.py
@@ -10,7 +10,7 @@ def atari():
         # network_kwargs=network_kwargs,
         # total_timesteps=100000,
 
-        print_freq=100,
+        # print_freq=100,
 
         learning_starts=10000,
         target_network_update_freq=1000,
@@ -22,12 +22,12 @@ def atari():
         load_path=None,
         checkpoint_freq=10000,
         checkpoint_path=None,
-        dueling=False,
-        param_noise=False,
+        # dueling=False,
+        # param_noise=False,
         # seed=None,
         callback=None,
 
-        prioritized_replay=False,
+        # prioritized_replay=False,
         prioritized_replay_alpha=0.6,
         prioritized_replay_beta0=0.4,
         prioritized_replay_beta_iters=None,
