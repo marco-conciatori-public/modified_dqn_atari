018fba3d6d0a5273ce88f09caa6a707bbd5ad62f
Marco Conciatori <irotaicnoc@gmail.com>
2019-03-27 15:36:21 +0100

    modified lower bound buffer experiences testing

--- baselines/deepq/replay_buffer.py
+++ baselines/deepq/replay_buffer.py
@@ -159,6 +159,8 @@ class LowerBoundReplayBuffer(ReplayBuffer):
             index -= 1
 
         estimated_rewards = q_values(np.array(old_observations))
+        print('compute_lb:')
+        print('estimated_rewards:', estimated_rewards)
         indexes, _ = test(actions, rewards, estimated_rewards)
 
         for i in indexes:
