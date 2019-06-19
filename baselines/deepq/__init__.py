cab60b98cfdc4f277fce4c22d791821f6c26768d
Marco Conciatori <irotaicnoc@gmail.com>
2019-06-19 17:14:22 +0200

    removed old import

--- baselines/deepq/__init__.py
+++ baselines/deepq/__init__.py
@@ -1,7 +1,7 @@
 from baselines.deepq import models  # noqa
 from baselines.deepq.build_graph import build_act, build_train  # noqa
 from baselines.deepq.deepq import learn, load_act  # noqa
-from baselines.deepq.replay_buffer import ReplayBuffer, PrioritizedReplayBuffer  # noqa
+from baselines.deepq.replay_buffer import ReplayBuffer  # noqa
 
 def wrap_atari_dqn(env):
     from baselines.common.atari_wrappers import wrap_deepmind
