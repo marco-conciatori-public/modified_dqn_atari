d68b2eea143efbe16b716f504705cc89858c378e
Marco Conciatori <irotaicnoc@gmail.com>
2019-06-20 15:47:58 +0200

    test alt q-values

--- baselines/deepq/deepq.py
+++ baselines/deepq/deepq.py
@@ -248,6 +248,7 @@ def learn(env,
             kwargs = {}
             update_eps = exploration.value(t)
 
+            print('np.array(obs):', np.array(obs))
             alt_q_values = q_func(np.array(obs), num_actions, scope="q_func", reuse=True)
             print('alt_q_values:', alt_q_values)
 
